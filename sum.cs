c6151592e508e88caeb786a304f6ccc87caf2ff6
pundalik Thogam <pthogam@gmail.com>
2023-03-21 15:10:09 +0530

    sum.cs

---
+++ sum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo
+{
+    public class sum
+
+    {
+        log log = new log();
+
+        public void Add(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                log.Logdebugthemessage("Entered the if condition");
+
+                log.Logerrormessage("The addition successsfull");
+            }
+            else
+            {
+                int c = a + b;
+                log.LogdedugThemessage("Entered the else condition");
+                log.loginfomessage("the addition successfull");
+
+
+            }
+        }
+    }
+}  
