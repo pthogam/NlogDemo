e7339c6b58ed5838d840b079c4e5eef96dad9f90
pundalik Thogam <pthogam@gmail.com>
2023-03-21 15:04:26 +0530

    log.cs

---
+++ log.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+using NlogDemo;
+
+
+namespace NlogDemo
+{
+    public class log
+    {
+        
+        Logger logger = LogManager. GetCurrentClassLogger();
+
+        public void logdebugThemessage(string message)
+        {
+            logger.Debug(message);
+        }
+        public void Logerrormessage(string message)
+        {
+            logger.Error(message);
+        }
+        public void Loginfomessage(string message)
+        {
+            logger.Info(message);
+
+        }
+        
+     
+        
+    }
+}
+
